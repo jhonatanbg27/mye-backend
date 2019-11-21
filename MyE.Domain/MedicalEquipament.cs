f1d76b85da57ed234e11b383352dcd04ec5aafcc
Jhonatan <jhonatanbg.12@gmail.com>
2019-11-20 21:16:52 -0500

    packet 3

---
+++ MyE.Domain/MedicalEquipament.cs
@@ -0,0 +1,11 @@
+namespace MyE.Domain
+{
+    public class MedicalEquipament
+    {
+        public int Id {get;set;}
+        public string StateDescription {get;set;}
+        public string Brand {get;set;}
+        public string Name {get;set;}
+
+    }
+}
